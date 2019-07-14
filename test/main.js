32e56f964f7003a7aa35fbf9683376773cbc68f9
Hen Shalom <xdrakolax@gmail.com>
2019-07-14 03:30:32 +0300

    fix test init

--- test/main.js
+++ test/main.js
@@ -4,7 +4,7 @@ import fs from "fs"
 import rimraf from "rimraf"
 import FileSystemManager from "../src"
 import { commonTest } from "anura-data-manager"
-import { newService, generalMocks } from "anura-data-manager/lib/tests/mocks"
+import { generalMocks, newService } from "anura-data-manager/lib/tests/mocks"
 const expect = chai.expect
 
 describe("FileSystem data manager ", function () {
@@ -12,7 +12,10 @@ describe("FileSystem data manager ", function () {
         beforeEach(function () {
             this.filePath = path.join(__dirname, `./tempFiles/temps_configs_${Math.random()}`) //reduce the probability of conflict 
             fs.mkdirSync(this.filePath)
-            this.dataManager = new FileSystemManager({ location: this.filePath, log: generalMocks.logMock })
+            this.dataManager = new FileSystemManager({
+                config: { STORE_LOCATION: this.filePath }
+                , log: generalMocks.logMock
+            })
             this.dataManager.createService(newService)
         });
 
